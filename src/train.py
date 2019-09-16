47915d7c57a981c930c5237fe7c3875a85b2f7ea
Akira Sosa <akirasosa@gmail.com>
2019-09-16 16:06:22 +0700

    format

--- src/train.py
+++ src/train.py
@@ -24,11 +24,11 @@ from utils.filters import *
 import argparse
 
 root = '../'  # This should be the root of the archive
-with open(os.path.join(root,'SETTINGS.json')) as f:
+with open(os.path.join(root, 'SETTINGS.json')) as f:
     settings = json.load(f)
 
 parser = argparse.ArgumentParser(description='Graph Transformer on Predicting Molecular properties')
-parser.add_argument('--data', type=str, default=os.path.join(root,settings['PROCESSED_DATA_DIR']),
+parser.add_argument('--data', type=str, default=os.path.join(root, settings['PROCESSED_DATA_DIR']),
                     help='location of the (processed) data')
 parser.add_argument('--n_layer', type=int, default=14,
                     help='number of total layers')
@@ -96,7 +96,7 @@ parser.add_argument('--log-interval', type=int, default=200,
                     help='report interval')
 parser.add_argument('--batch_chunk', type=int, default=1,
                     help='batch chunking')
-parser.add_argument('--work_dir', default=os.path.join(root,settings['MODEL_DIR'],'CHAMP-GT'), type=str,
+parser.add_argument('--work_dir', default=os.path.join(root, settings['MODEL_DIR'], 'CHAMP-GT'), type=str,
                     help='experiment directory.')
 parser.add_argument('--restart', action='store_true',
                     help='restart training from the saved checkpoint')
@@ -133,7 +133,7 @@ if args.d_embed < 0:
 args.work_dir = '{}'.format(args.work_dir)
 args.work_dir = os.path.join(args.work_dir, time.strftime('%Y%m%d-%H%M%S'))
 logging = create_exp_dir(args.work_dir,
-    scripts_to_save=['train.py', 'graph_transformer.py'], debug=args.debug)
+                         scripts_to_save=['train.py', 'graph_transformer.py'], debug=args.debug)
 
 # Set the random seed manually for reproducibility.
 np.random.seed(args.seed)
@@ -152,13 +152,14 @@ APEX_AVAILABLE = False
 if args.fp16:
     try:
         from apex import amp
+
         APEX_AVAILABLE = True
     except ModuleNotFoundError:
         raise ImportError("Please install apex from https://www.github.com/nvidia/apex to run this example.")
         APEX_AVAILABLE = False
 
 if len(args.name) == 0:
-    args.name = f'{args.d_model}x{args.n_layer}_dr{int(args.dropout*100)}_nhead{args.n_head}'
+    args.name = f'{args.d_model}x{args.n_layer}_dr{int(args.dropout * 100)}_nhead{args.n_head}'
 
 #####################################################################
 #
@@ -173,10 +174,10 @@ if val_dataset is not None:
     assert args.mode != "_full", "You should NOT use the validation set when not in _full mode"
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
 
-NUM_ATOM_TYPES = [int(train_dataset.tensors[1][:,:,i].max()) for i in range(3)]   # Atom hierarchy has 3 levels
-NUM_BOND_TYPES = [int(train_dataset.tensors[3][:,:,i].max()) for i in range(3)]   # Bond hierarchy has 3 levels
-NUM_TRIPLET_TYPES = [int(train_dataset.tensors[5][:,:,i].max()) for i in range(2)]  # Triplet hierarchy has 2 levels
-NUM_QUAD_TYPES = [int(train_dataset.tensors[7][:,:,i].max()) for i in range(1)]   # Quad hierarchy has only 1 level
+NUM_ATOM_TYPES = [int(train_dataset.tensors[1][:, :, i].max()) for i in range(3)]  # Atom hierarchy has 3 levels
+NUM_BOND_TYPES = [int(train_dataset.tensors[3][:, :, i].max()) for i in range(3)]  # Bond hierarchy has 3 levels
+NUM_TRIPLET_TYPES = [int(train_dataset.tensors[5][:, :, i].max()) for i in range(2)]  # Triplet hierarchy has 2 levels
+NUM_QUAD_TYPES = [int(train_dataset.tensors[7][:, :, i].max()) for i in range(1)]  # Quad hierarchy has only 1 level
 NUM_BOND_ORIG_TYPES = 8
 MAX_BOND_COUNT = args.max_bond_count
 print(f"Atom hierarchy: {NUM_ATOM_TYPES}")
@@ -199,7 +200,7 @@ if len(args.load) > 0:
     model = torch.load(args.load)
 else:
     model = GraphTransformer(dim=args.d_model, n_layers=args.n_layer, d_inner=args.d_inner,
-                             fdim = args.feature_dim, final_dim=args.final_dim, dropout=args.dropout,
+                             fdim=args.feature_dim, final_dim=args.final_dim, dropout=args.dropout,
                              dropatt=args.dropatt, final_dropout=args.final_dropout, n_head=args.n_head,
                              num_atom_types=NUM_ATOM_TYPES,
                              num_bond_types=NUM_BOND_TYPES,
@@ -212,7 +213,6 @@ else:
                              wnorm=args.wnorm,
                              use_quad=args.use_quad).to(device)
 args.n_all_param = sum([p.nelement() for p in model.parameters() if p.requires_grad])
-
 
 # 2. Initialize optimizer and learning rate scheduler
 optimizer = getattr(optim if args.optim != "RAdam" else radam, args.optim)(model.parameters(), lr=args.lr)
@@ -230,6 +230,8 @@ elif args.scheduler == 'inv_sqrt':
             return 1.
         else:
             return 1. / (step ** 0.5) if step > args.warmup_step else step / (args.warmup_step ** 1.5)
+
+
     scheduler = optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lr_lambda)
 elif args.scheduler == 'dev_perf':
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=args.decay_rate,
@@ -264,7 +266,7 @@ logging('=' * 60)
 for k, v in args.__dict__.items():
     logging('    - {} : {}'.format(k, v))
 logging('=' * 60)
-logging(f'#params = {args.n_all_param/1e6:.2f}M')
+logging(f'#params = {args.n_all_param / 1e6:.2f}M')
 
 
 #####################################################################
@@ -277,15 +279,15 @@ def loss(y_pred, y, x_bond):
     y_pred_pad = torch.cat([torch.zeros(y_pred.shape[0], 1, y_pred.shape[2], device=y_pred.device), y_pred], dim=1)
 
     # Note: The [:,:,1] below should match the num_bond_types[1]*final_dim in graph transformer
-    y_pred_scaled = y_pred_pad.gather(1,x_bond[:,:,1][:,None,:])[:,0,:] * y[:,:,2] + y[:,:,1]
-    abs_dy = (y_pred_scaled - y[:,:,0]).abs()
-    loss_bonds = (x_bond[:,:,0] > 0)
-    abs_err = abs_dy.masked_select(loss_bonds & (y[:,:,3] > 0)).sum()
+    y_pred_scaled = y_pred_pad.gather(1, x_bond[:, :, 1][:, None, :])[:, 0, :] * y[:, :, 2] + y[:, :, 1]
+    abs_dy = (y_pred_scaled - y[:, :, 0]).abs()
+    loss_bonds = (x_bond[:, :, 0] > 0)
+    abs_err = abs_dy.masked_select(loss_bonds & (y[:, :, 3] > 0)).sum()
 
-    type_dy = [abs_dy.masked_select(x_bond[:,:,0] == i) for i in range(1,NUM_BOND_ORIG_TYPES+1)]
+    type_dy = [abs_dy.masked_select(x_bond[:, :, 0] == i) for i in range(1, NUM_BOND_ORIG_TYPES + 1)]
     if args.champs_loss:
         type_err = torch.cat([t.sum().view(1) for t in type_dy], dim=0)
-        type_cnt = torch.cat([torch.sum(x_bond[:,:,0] == i).view(1) for i in range(1,NUM_BOND_ORIG_TYPES+1)])
+        type_cnt = torch.cat([torch.sum(x_bond[:, :, 0] == i).view(1) for i in range(1, NUM_BOND_ORIG_TYPES + 1)])
     else:
         type_err = torch.tensor([t.sum() for t in type_dy])
         type_cnt = torch.tensor([len(t) for t in type_dy])
@@ -296,7 +298,8 @@ def epoch(loader, model, opt=None, ep=-1):
     global train_step
     model.eval() if opt is None else model.train()
     dev = next(model.parameters()).device
-    abs_err, type_err, type_cnt = 0.0, torch.zeros(NUM_BOND_ORIG_TYPES), torch.zeros(NUM_BOND_ORIG_TYPES, dtype=torch.long)
+    abs_err, type_err, type_cnt = 0.0, torch.zeros(NUM_BOND_ORIG_TYPES), torch.zeros(NUM_BOND_ORIG_TYPES,
+                                                                                     dtype=torch.long)
     log_interval = args.log_interval
 
     with torch.enable_grad() if opt else torch.no_grad():
@@ -307,7 +310,7 @@ def epoch(loader, model, opt=None, ep=-1):
                 x_atom.to(dev), x_atom_pos.to(dev), x_bond.to(dev), x_bond_dist.to(dev), \
                 x_triplet.to(dev), x_triplet_angle.to(dev), x_quad.to(dev), x_quad_angle.to(dev), y.to(dev)
 
-            x_bond, x_bond_dist, y = x_bond[:, :MAX_BOND_COUNT], x_bond_dist[:, :MAX_BOND_COUNT], y[:,:MAX_BOND_COUNT]
+            x_bond, x_bond_dist, y = x_bond[:, :MAX_BOND_COUNT], x_bond_dist[:, :MAX_BOND_COUNT], y[:, :MAX_BOND_COUNT]
 
             if opt:
                 # Put this here so that the batch_chunk setting will work
@@ -323,13 +326,13 @@ def epoch(loader, model, opt=None, ep=-1):
                 b_abs_err = 0
                 b_type_err = 0
                 b_type_cnt = 0
-                types_cnt = sum([(x_bond[:,:,0] == i).sum() for i in range(1,NUM_BOND_ORIG_TYPES+1)])
+                types_cnt = sum([(x_bond[:, :, 0] == i).sum() for i in range(1, NUM_BOND_ORIG_TYPES + 1)])
                 for i in range(args.batch_chunk):
-                    mini = slice(i*mbsz,(i+1)*mbsz)
+                    mini = slice(i * mbsz, (i + 1) * mbsz)
                     y_pred_mb, _ = para_model(x_atom[mini], x_atom_pos[mini], x_bond[mini], x_bond_dist[mini],
                                               x_triplet[mini], x_triplet_angle[mini], x_quad[mini], x_quad_angle[mini])
                     mb_abs_err, mb_type_err, mb_type_cnt = loss(y_pred_mb, y[mini], x_bond[mini])
-                    b_abs_err += mb_abs_err.detach()       # No need to average, as it's sum
+                    b_abs_err += mb_abs_err.detach()  # No need to average, as it's sum
                     b_type_err += mb_type_err.detach()
                     b_type_cnt += mb_type_cnt.detach()
                     mb_raw_loss = mb_abs_err / types_cnt.float()
@@ -341,7 +344,8 @@ def epoch(loader, model, opt=None, ep=-1):
                     else:
                         mb_raw_loss.backward()
             else:
-                y_pred, _ = para_model(x_atom, x_atom_pos, x_bond, x_bond_dist, x_triplet, x_triplet_angle, x_quad, x_quad_angle)
+                y_pred, _ = para_model(x_atom, x_atom_pos, x_bond, x_bond_dist, x_triplet, x_triplet_angle, x_quad,
+                                       x_quad_angle)
                 b_abs_err, b_type_err, b_type_cnt = loss(y_pred, y, x_bond)
 
             abs_err += b_abs_err.detach()
@@ -358,10 +362,11 @@ def epoch(loader, model, opt=None, ep=-1):
                 elif args.scheduler == 'cosine':
                     scheduler.step(train_step)
                 if args.batch_chunk == 1:
-                    raw_loss = b_abs_err/b_type_cnt.sum()
+                    raw_loss = b_abs_err / b_type_cnt.sum()
                     if args.champs_loss:
                         nonzero_indices = b_type_cnt.nonzero()
-                        raw_loss = torch.log((b_type_err[nonzero_indices] / b_type_cnt[nonzero_indices].float()) + 1e-9).mean()
+                        raw_loss = torch.log(
+                            (b_type_err[nonzero_indices] / b_type_cnt[nonzero_indices].float()) + 1e-9).mean()
                     if APEX_AVAILABLE:
                         with amp.scale_loss(raw_loss, opt) as scaled_loss:
                             scaled_loss.backward()
@@ -372,12 +377,14 @@ def epoch(loader, model, opt=None, ep=-1):
 
                 if batch_id % log_interval == 0:
                     avg_loss = torch.log(total_loss / log_interval).mean().item()
-                    logging(f"Epoch {ep:2d} | Step {train_step} | lr {opt.param_groups[0]['lr']:.7f} | Error {avg_loss:.5f}")
+                    logging(
+                        f"Epoch {ep:2d} | Step {train_step} | lr {opt.param_groups[0]['lr']:.7f} | Error {avg_loss:.5f}")
 
                     total_loss = 0
 
     torch.cuda.empty_cache()
-    return abs_err / type_cnt.sum(), torch.log(type_err / type_cnt.float()).mean(), torch.log(type_err / type_cnt.float())
+    return abs_err / type_cnt.sum(), torch.log(type_err / type_cnt.float()).mean(), torch.log(
+        type_err / type_cnt.float())
 
 
 if __name__ == '__main__':
@@ -391,7 +398,7 @@ if __name__ == '__main__':
         if val_dataset is not None:
             _, err, type_err = epoch(val_loader, model)
             end = time.time()
-            logging(f"Epoch {i:2d} | Time {end-start:.2f} sec | Validation Error {err:.5f}")
+            logging(f"Epoch {i:2d} | Time {end - start:.2f} sec | Validation Error {err:.5f}")
             type_err_str = [eval(f"{elem:.2f}") for elem in type_err.tolist()]
             logging(f"Epoch {i:2d} | Validation Error (by Type) \n {type_err_str}")
             if err < best_val_err:
@@ -399,14 +406,14 @@ if __name__ == '__main__':
                     torch.save(model, f)
                 with open(os.path.join(args.work_dir, 'optimizer.pt'), 'wb') as f:
                     torch.save(optimizer.state_dict(), f)
-                save_path = os.path.join(root,settings['MODEL_DIR'],f"ckpt/graph_transformer_part_{args.name}.ckpt")
+                save_path = os.path.join(root, settings['MODEL_DIR'], f"ckpt/graph_transformer_part_{args.name}.ckpt")
                 logging(f"Saving model at {save_path}!")
                 best_val_err = err
                 torch.save(model, save_path)
         else:
             end = time.time()
-            save_path = os.path.join(root,settings['MODEL_DIR'],f"ckpt/graph_transformer_{args.name}.ckpt")
-            logging(f"Saving model at {save_path} (without validation error)! Time: {end-start:.2f} sec")
+            save_path = os.path.join(root, settings['MODEL_DIR'], f"ckpt/graph_transformer_{args.name}.ckpt")
+            logging(f"Saving model at {save_path} (without validation error)! Time: {end - start:.2f} sec")
             torch.save(model, save_path)
         if args.scheduler == 'dev_perf':
             scheduler.step(err)
